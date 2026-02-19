9e53cf397fffc43d74e638595e8071c32a0fa4e2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-20 00:15:39 +0500

    update

--- server/config/settings.py
+++ server/config/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER" : config("DB_AUTHOR"),
+        "HOST" : config("DB_HOST"),
+        "PORT" :config("DB_PORT"), 
+        "PASSWORD" : config("DB_PASSWORD"),
     }
 }
 
