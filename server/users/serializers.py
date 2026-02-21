e6f6b858dce6c69158f1b977b110e957ceeb57b4
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-21 21:29:53 +0500

    update

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -1,10 +1,30 @@
 from rest_framework import serializers
 from .models import User
 
+
 class SingUpSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(read_only= True)
-    
-    class Meta :
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
         model = User
-        fields = '__all__'
-        
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "created",
+            "updated",
+            "last_login",
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['tokens'] = instance.token()
+        return data
