25d8cb77036027adb4daf1ed7800d3c1126e2e7e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-24 23:52:05 +0500

    updtae

--- server/chat/consumerClone.py
+++ server/chat/consumerClone.py
@@ -5,29 +5,32 @@ import json
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.group_name = f"chat_{self.room_name}"
+
         if self.scope["user"].is_anonymous:
             await self.close()
             return
 
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.channel_layer.group_add(self.group_name, self.room_name)
         await self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.group_name, self.room_name)
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.load(text_data)
         message = data["message"]
+
         await self.save_message(self.room_name, message)
-        await self.channel_layer.group_send(
+        await self.channel_layer.send(
             self.group_name,
             {
                 "type": "chat_message",
-                "message": data["message"],
-                "author": self.scope["user"].username
+                "message": message,
+                "author": self.scope["user"].username,
             },
         )
 
@@ -39,9 +42,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     @database_sync_to_async
-    def save_message(self, room, content):
+    def save_message(self, room_name, content):
         Message.objects.create(
-            room=room,
-            content=content,
-            author=self.scope["user"],
+            room=room_name, author=self.scope["user"], content=content
         )
--- server/chat/middleware.py
+++ server/chat/middleware.py
@@ -2,30 +2,30 @@ from urllib.parse import parse_qs
 from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth import get_user_model
+from channels.middleware import BaseMiddleware
 from channels.db import database_sync_to_async
 
 User = get_user_model()
+
 
 @database_sync_to_async
 def get_user(token_key):
     try:
         token = AccessToken(token_key)
-        user_id = token['user_id']
+        user_id = token["user_id"]
         return User.objects.get(id=user_id)
     except Exception:
         return AnonymousUser()
 
-class JWTAuthMiddleware:
-    def __init__(self, app):
-        self.app = app
 
+class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        query_string = parse_qs(scope['query_string'].decode())
-        token = query_string.get('token', [None])[0]
+        query_string = parse_qs(scope["query_string"].decode())
+        token = query_string.get("token")
 
         if token:
-            scope['user'] = await get_user(token)
+            scope["user"] = await get_user(token[0])
         else:
-            scope['user'] = AnonymousUser()
+            scope["user"] = AnonymousUser()
 
-        return await self.app(scope, receive, send)
+        return await super().__call__(scope, receive, send)
